273b8e0ca6d1e811f5c7c3003a8b0d1b169c11eb
Claudio <lapertosa.c@gmail.com>
2021-05-19 20:10:06 +0200

    Planet controller

---
+++ WebUI/Controllers/PlanetController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class PlanetController : BaseController
+    {
+        private readonly IPlanetRepository _planetRepository;
+
+        public PlanetController(IPlanetRepository planetRepository)
+        {
+            _planetRepository = planetRepository;
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult<IEnumerable<Planet>>> GetAll()
+        {
+            return Ok(await _planetRepository.GetAllPlanetsAsync());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Planet>> GetPlanet(int id)
+        {
+            return Ok(await _planetRepository.GetPlanetInfoAsync(id));
+        }
+    }
+}
